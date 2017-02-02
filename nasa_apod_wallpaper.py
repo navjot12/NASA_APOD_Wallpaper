c36767830e73f25674fcf464e73258d8f2ffe7ed
Navjot Singh <singh.navjot12@gmail.com>
2017-02-03 00:29:09 +0530

    Birthday Present

---
+++ nasa_apod_wallpaper.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import os
+import requests
+import time
+from random import randint
+from bs4 import BeautifulSoup as BS
+
+user = 'arpit'
+
+url = 'https://apod.nasa.gov/apod/'
+r = requests.get(url)
+soup = BS(r.text, "html.parser")
+flag = 0
+
+while flag is 0:
+	try:
+		image = soup.find_all('img')[0]['src']
+		flag = 1
+	except:					#Getting an image from archives if currently a non-image content is posted
+		url2 = 'https://apod.nasa.gov/apod/archivepix.html'
+		r = requests.get(url2)
+		soup = BS(r.text, "html.parser")
+		soup = soup.find_all('a')
+		x = randint(0, len(soup))
+		url2 = url + soup[x]['href']
+		r = requests.get(url2)
+		soup = BS(r.text, "html.parser")
+
+url = url + image
+
+os.system('/usr/bin/gsettings set org.gnome.desktop.background picture-uri "myloxyloto"')
+os.system('wget -O /home/'+user+'/Pictures/desktop_background.jpg ' + url)
+
+time.sleep(15)				#15 seconds time to allow proper download of picture
+flag = 5
+if flag is 5:
+	os.system('/usr/bin/gsettings set org.gnome.desktop.background picture-uri file:///home/'+user+'/Pictures/desktop_background.jpg')
+	os.system('/usr/bin/gsettings set org.gnome.desktop.background picture-options "stretched"')
---
+++ readMe.txt
@@ -0,0 +1,27 @@
+NASA APOD Wallpaper
+
+This script will set your wallpaper to NASA's Astronomy Picture of the Day available at https://apod.nasa.gov/apod/.
+If a non-image item is the current APOD, the script will choose any image from the archives.
+
+To use:
+1. Clone this repository.
+2. In nasa_apod_wallpaper.py, edit "user" variable to your own username.
+3. Run nasa_apod_wallpaper.py!
+
+Note: The script sleeps for 15 seconds to allow complete download the APOD.
+
+To have this script run daily, run the following commands in terminal:
+
+```crontab -e'''
+
+This will open a file to be edited in terminal. Enter the following statement in it
+
+```* */12 * * * /path/to/python nasa_apod_wallpaper.py```
+
+where 'path/to/' represents the path to nasa_apod_wallpaper.py script.
+
+Press ```ctrl+x``` to exit file editing mode and press ```Y``` to save changes.
+
+This will run the script in your system in every 12 hours!
+
+PS: Happy birthday to my friend Arpit Gogia (@arpitgogia) who was the inspiration behind this!
